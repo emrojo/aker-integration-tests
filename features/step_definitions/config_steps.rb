db17effba21701d6f31848eaac50ecf13cb6a061
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-11-30 16:29:45 +0000

    Get user info from environment variable

--- features/step_definitions/config_steps.rb
+++ features/step_definitions/config_steps.rb
@@ -37,8 +37,8 @@ end
 Given(/^I am logged in$/) do
   if INTEGRATION_TESTS_CONFIG['perform_login']
     if page.has_content?("Aker Log in")
-      fill_in("Username/Email", with: INTEGRATION_TESTS_CONFIG['username'])
-      fill_in("Password", with: INTEGRATION_TESTS_CONFIG['password'])
+      fill_in("Username/Email", with: ENV['USERNAME'])
+      fill_in("Password", with: ENV['PASSWORD'])
       click_on('Log in')
     end
   end
